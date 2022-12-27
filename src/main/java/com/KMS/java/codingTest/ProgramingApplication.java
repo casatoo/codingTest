a867777d8bc04b20308087c397adb6bf3eb08fa5
casatoo <casato6666@gmail.com>
2022-12-28 04:03:29 +0900

    first commit

---
+++ src/main/java/com/KMS/java/codingTest/ProgramingApplication.java
@@ -0,0 +1,13 @@
+package com.KMS.java.codingTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KMS/java/codingTest/ProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KMS.java.codingTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
